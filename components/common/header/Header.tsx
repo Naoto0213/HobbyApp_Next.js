ef49b63272c217a597de8b2360597d20d3f4bd04
githubアカウントのユーザー <m.naotomachida@gmail.com>
2021-07-22 04:10:18 +0900

    Headerを作成しました

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -1,18 +1,19 @@
 import Link from "next/Link";
 import { HEADER } from "../../../config/header";
+
 const Header = () => {
   return (
     <>
-      <header className="w-screen bg-purple-600 py-6 px-24 flex flex items-center justify-between">
+      <header className="w-screen bg-purple-600 py-6 px-2 sm:px-16 sm:px-24 flex flex-col sm:flex-row items-center sm:justify-between justify-center">
         <div className="WhiteText">
           <Link href="/">
             <a>ホビーライク</a>
           </Link>
         </div>
-        <ul className="flex">
+        <ul className="flex justify-center items-center pt-3 sm:pt-0">
           {HEADER.NAV.map((item) => {
             return (
-              <li className="pr-6 WhiteText">
+              <li className="px-3 WhiteText ">
                 <Link href={item.link}>
                   <a>{item.label}</a>
                 </Link>
